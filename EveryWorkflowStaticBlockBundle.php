8ec0d05e187deb80ec99c7cc720b96cffc97587d
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:01:23 +0000

    Added ?ExtensionInterface

--- EveryWorkflowStaticBlockBundle.php
+++ EveryWorkflowStaticBlockBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\StaticBlockBundle;
 
 use EveryWorkflow\StaticBlockBundle\DependencyInjection\StaticBlockExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowStaticBlockBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new StaticBlockExtension();
     }
